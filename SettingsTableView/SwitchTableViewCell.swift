5995dd888aaf6f61d7230dee16b6a3a1bdd8d32d
fluushx <62612386+fluushx@users.noreply.github.com>
2021-08-10 21:10:11 -0400

    add airplane mod

---
+++ SettingsTableView/SwitchTableViewCell.swift
@@ -0,0 +1,101 @@
+//
+//  SwitchTableViewCell.swift
+//  SettingsTableView
+//
+//  Created by Felipe Ignacio Zapata Riffo on 10-08-21.
+//
+
+import UIKit
+
+//
+//  SettingTableViewCell.swift
+//  SettingsTableView
+//
+//  Created by Felipe Ignacio Zapata Riffo on 10-08-21.
+//
+
+import UIKit
+
+class SwitchTableViewCell: UITableViewCell {
+    static let identifier = "SwitchTableViewCell"
+    
+    private let iconContainer: UIView = {
+        let view = UIView()
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 10
+        view.layer.masksToBounds = true
+        return view
+    }()
+    
+    private let iconImageView : UIImageView = {
+        let imageView = UIImageView()
+        imageView.tintColor = .white
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private let label:UILabel = {
+       let label = UILabel()
+        label.numberOfLines = 1
+       return label
+    }()
+    private let mySwitch : UISwitch = {
+       let mySwitch = UISwitch()
+        mySwitch.onTintColor = .systemBlue
+        
+        return mySwitch
+    }()
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(label)
+        contentView.addSubview(iconContainer)
+        contentView.addSubview(iconImageView)
+        contentView.addSubview(mySwitch)
+         
+         contentView.clipsToBounds = true
+        accessoryType = .none
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        let size:CGFloat = contentView.frame.size.height - 12
+        iconContainer.frame = CGRect(x: 15,
+                                     y: 6,
+                                     width: size,
+                                     height: size)
+        
+        let imageSize:CGFloat = size/1.5
+        iconImageView.frame = CGRect(x: 0,
+                                     y: 0,
+                                     width: imageSize,
+                                     height: imageSize)
+        iconImageView.center = iconContainer.center
+        label.frame = CGRect(x: 25 + iconContainer.frame.size.width,
+                             y: 0,
+                             width: contentView.frame.size.width - 15 - iconContainer.frame.size.width,
+                             height: contentView.frame.size.height)
+        mySwitch.sizeToFit()
+        mySwitch.frame = CGRect(x: contentView.frame.size.width - mySwitch.frame.size.width - 20, y: (contentView.frame.size.height - mySwitch.frame.size.height)/2,
+                                width: mySwitch.frame.size.width,
+                                height: mySwitch.frame.size.height)
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        iconImageView.image = nil
+        iconContainer.backgroundColor = nil
+        label.text =  nil
+        mySwitch.isOn = false
+    }
+    public func configure (with model:SettingsSwitchOption){
+        label.text = model.title
+        iconImageView.image = model.icon
+        iconContainer.backgroundColor = model.iconBackgroundColor
+        mySwitch.isOn = model.isOn
+    }
+}
+
--- SettingsTableView/ViewController.swift
+++ SettingsTableView/ViewController.swift
@@ -9,7 +9,20 @@ import UIKit
 
 struct Section {
     let title: String
-    let options: [SettingsOption]
+    let options: [SettingsOptionType]
+}
+
+enum SettingsOptionType {
+    case staticCell(model:SettingsOption)
+    case switfhCell(model:SettingsSwitchOption)
+}
+
+struct SettingsSwitchOption {
+    let title:String
+    let icon: UIImage?
+    let iconBackgroundColor: UIColor
+    let handler : (()->Void)
+    var isOn: Bool
 }
 struct SettingsOption {
     let title:String
@@ -18,12 +31,14 @@ struct SettingsOption {
     let handler : (()->Void)
 }
 
+
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     var models = [Section]()
     
     private let tableView:UITableView = {
         let table = UITableView(frame: .zero, style: .grouped)
         table.register(SettingTableViewCell.self, forCellReuseIdentifier: SettingTableViewCell.identifier)
+        table.register(SwitchTableViewCell.self, forCellReuseIdentifier: SwitchTableViewCell.identifier)
         return table
     }()
 
@@ -37,37 +52,44 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         tableView.frame = view.bounds
     }
     func configure(){
+        
         models.append(Section(title: "General", options: [
-            SettingsOption(title: "Wifi", icon: UIImage(systemName: "house"), iconBackgroundColor: .systemPink, handler: {
-            }),
-            SettingsOption(title: "Bluetooh", icon: UIImage(systemName: "bluetooh"), iconBackgroundColor: .link, handler: {
-            }),
-            SettingsOption(title: "Airplane Mode", icon: UIImage(systemName: "airplane"), iconBackgroundColor: .systemGreen, handler: {
-            }),
-            SettingsOption(title: "Icloud", icon: UIImage(systemName: "cloud"), iconBackgroundColor: .systemOrange, handler: {
-            })
+            .switfhCell(model: SettingsSwitchOption(title: "Air Plane", icon: UIImage(systemName: "airplane"), iconBackgroundColor: .systemBlue, handler: {
+                
+            }, isOn: true))
+        ]))
+        
+        models.append(Section(title: "General", options: [
+            .staticCell(model:   SettingsOption(title: "Wifi", icon: UIImage(systemName: "wifi"), iconBackgroundColor: .systemPink, handler: {
+            })),
+            .staticCell(model:   SettingsOption(title: "Bluetooh", icon: UIImage(systemName: "bluetooh"), iconBackgroundColor: .link, handler: {
+            })),
+            .staticCell(model:   SettingsOption(title: "Airplane Mode", icon: UIImage(systemName: "airplane"), iconBackgroundColor: .systemGreen, handler: {
+            })),
+            .staticCell(model:   SettingsOption(title: "Icloud", icon: UIImage(systemName: "cloud"), iconBackgroundColor: .systemOrange, handler: {
+            }))
         ]))
         
         models.append(Section(title: "Information", options: [
-            SettingsOption(title: "Wifi", icon: UIImage(systemName: "house"), iconBackgroundColor: .systemPink, handler: {
-            }),
-            SettingsOption(title: "Bluetooh", icon: UIImage(systemName: "bluetooh"), iconBackgroundColor: .link, handler: {
-            }),
-            SettingsOption(title: "Airplane Mode", icon: UIImage(systemName: "airplane"), iconBackgroundColor: .systemGreen, handler: {
-            }),
-            SettingsOption(title: "Icloud", icon: UIImage(systemName: "cloud"), iconBackgroundColor: .systemOrange, handler: {
-            })
+            .staticCell(model:   SettingsOption(title: "Wifi", icon: UIImage(systemName: "wifi"), iconBackgroundColor: .systemPink, handler: {
+            })),
+            .staticCell(model:   SettingsOption(title: "Bluetooh", icon: UIImage(systemName: "bluetooh"), iconBackgroundColor: .link, handler: {
+            })),
+            .staticCell(model:   SettingsOption(title: "Airplane Mode", icon: UIImage(systemName: "airplane"), iconBackgroundColor: .systemGreen, handler: {
+            })),
+            .staticCell(model:   SettingsOption(title: "Icloud", icon: UIImage(systemName: "cloud"), iconBackgroundColor: .systemOrange, handler: {
+            }))
         ]))
         
         models.append(Section(title: "Apps", options: [
-            SettingsOption(title: "Wifi", icon: UIImage(systemName: "house"), iconBackgroundColor: .systemPink, handler: {
-            }),
-            SettingsOption(title: "Bluetooh", icon: UIImage(systemName: "bluetooh"), iconBackgroundColor: .link, handler: {
-            }),
-            SettingsOption(title: "Airplane Mode", icon: UIImage(systemName: "airplane"), iconBackgroundColor: .systemGreen, handler: {
-            }),
-            SettingsOption(title: "Icloud", icon: UIImage(systemName: "cloud"), iconBackgroundColor: .systemOrange, handler: {
-            })
+            .staticCell(model:   SettingsOption(title: "Wifi", icon: UIImage(systemName: "wifi"), iconBackgroundColor: .systemPink, handler: {
+            })),
+            .staticCell(model:   SettingsOption(title: "Bluetooh", icon: UIImage(systemName: "bluetooh"), iconBackgroundColor: .link, handler: {
+            })),
+            .staticCell(model:   SettingsOption(title: "Airplane Mode", icon: UIImage(systemName: "airplane"), iconBackgroundColor: .systemGreen, handler: {
+            })),
+            .staticCell(model:   SettingsOption(title: "Icloud", icon: UIImage(systemName: "cloud"), iconBackgroundColor: .systemOrange, handler: {
+            }))
         ]))
     }
     
@@ -85,16 +107,35 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: SettingTableViewCell.identifier, for: indexPath) as! SettingTableViewCell
         let model = models[indexPath.section].options[indexPath.row]
-        cell.configure(with: model)
-        return cell
+        switch model.self {
+            case .staticCell(let model):
+                guard let cell = tableView.dequeueReusableCell(withIdentifier: SettingTableViewCell.identifier, for: indexPath) as? SettingTableViewCell else {
+                    return UITableViewCell()
+                }
+                cell.configure(with: model)
+                return cell
+            case .switfhCell(let model):
+                guard let cell = tableView.dequeueReusableCell(withIdentifier: SwitchTableViewCell.identifier, for: indexPath) as? SwitchTableViewCell else {
+                    return UITableViewCell()
+                }
+                cell.configure(with: model)
+                return cell
+        }
+        
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        let model =  models[indexPath.section].options[indexPath.row]
-        model.handler()
+        let type =  models[indexPath.section].options[indexPath.row]
+        
+        switch type.self {
+            case .staticCell(let model):
+                model.handler()
+            case .switfhCell(let model):
+                model.handler()
+                
+        }
     }
 }
 
